d910de0412dc641100cfda9bad921f4aa1949854
Dobbymin <gmin990624@gmail.com>
2025-01-19 13:42:49 +0900

    feat(global-style): global style 적용 - custom color 적용 - font 적용 - background color 및 padding, 정렬 적용

---
+++ src/widgets/themes/global/global-style.ts
@@ -0,0 +1,34 @@
+import { extendTheme } from '@chakra-ui/react';
+import { Button } from '@widgets/layouts';
+
+const config = {
+	useSystemColorMode: true,
+	initialColorMode: 'light',
+	colors: {
+		custom: {
+			gray: '#F7F9FB',
+			blue: '#0076BF',
+		},
+	},
+	components: {
+		Button,
+	},
+	styles: {
+		global: {
+			'@font-face': {
+				fontFamily: 'Pretendard-Regular',
+				src: `url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff')`,
+				fontStyle: 'normal',
+			},
+			'html, body': {
+				fontFamily: `"Pretendard-Regular","Apple SD Gothic Neo", Sans-serif`,
+				fontSize: 'md',
+				bg: 'custom.gray',
+				textAlign: 'center',
+				px: '150px',
+			},
+		},
+	},
+};
+
+export const GlobalStyle = extendTheme(config);
