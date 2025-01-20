071179c2fc74804e22f2ac3df7c395ce0c6374b5
Dobbymin <gmin990624@gmail.com>
2025-01-20 16:40:36 +0900

    fix(router): 페이지 라우터 기능 구현 - 각 페이지에 route 경로 적용 - 모든 경로 상수화 된 경로로 수정 - Layout 적용

--- src/app/routes/router.tsx
+++ src/app/routes/router.tsx
@@ -1,11 +1,54 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { MainPage } from '@pages/main';
+import { Layout } from '@widgets/layouts';
+import { PostDetailPage } from '@pages/post-detail';
+import { PostEditPage } from '@pages/post-edit';
+import { PostWritePage } from '@pages/post-write';
+import { ChatPage } from '@pages/chat';
+import { CodeReviewPage } from '@pages/code-review';
+import { Mypage } from '@pages/mypage';
+import { ErrorPage } from '@pages/error';
+import { RouterPath } from '@shared/constants';
 
 const router = createBrowserRouter([
 	{
-		path: '/',
-		element: <MainPage />,
+		path: RouterPath.ROOT,
+		element: <Layout />,
+		children: [
+			{
+				path: RouterPath.MAIN,
+				element: <MainPage />,
+			},
+			{
+				path: RouterPath.POST_DETAIL,
+				element: <PostDetailPage />,
+			},
+			{
+				path: RouterPath.POST_EDIT,
+				element: <PostEditPage />,
+			},
+			{
+				path: RouterPath.POST_WRITE,
+				element: <PostWritePage />,
+			},
+			{
+				path: RouterPath.CHAT,
+				element: <ChatPage />,
+			},
+			{
+				path: RouterPath.CODE_REVIEW,
+				element: <CodeReviewPage />,
+			},
+			{
+				path: RouterPath.MYPAGE,
+				element: <Mypage />,
+			},
+			{
+				path: RouterPath.ERROR,
+				element: <ErrorPage />,
+			},
+		],
 	},
 ]);
 
