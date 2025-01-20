219a1736ed735a5b51041db626f28d8a9ca5c69d
Dobbymin <gmin990624@gmail.com>
2025-01-21 03:05:55 +0900

    feat: Application provider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { Routes } from '@app/routes';
-import { ChakraProvider } from '@chakra-ui/react';
-import { ReactQueryClientProvider } from '@shared/lib';
-import { GlobalStyle } from '@widgets/themes';
+import { ApplicationProvider } from '@shared/providers';
 
 const App = () => {
 	return (
-		<ReactQueryClientProvider>
-			<ChakraProvider theme={GlobalStyle}>
-				<Routes />
-			</ChakraProvider>
-		</ReactQueryClientProvider>
+		<ApplicationProvider>
+			<Routes />
+		</ApplicationProvider>
 	);
 };
 
