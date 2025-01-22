158a2d902dcb496db69106e75f305eb6e9df468a
Dobbymin <gmin990624@gmail.com>
2025-01-22 17:32:00 +0900

    feat(modal): auth 관련 modal layout 구현

---
+++ src/widgets/modals/AuthModal.tsx
@@ -0,0 +1,80 @@
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Flex,
+  Text,
+} from '@chakra-ui/react';
+
+type AuthModalProps = {
+  modalTitle: string;
+  buttonText: string;
+  children: React.ReactNode;
+  isOpen: boolean;
+  navigateText: string;
+  navigateModal: string;
+  scrollBehavior?: 'inside' | 'outside';
+  onClose: () => void;
+  modalType: string;
+  setModalType: (modalType: string) => void;
+  setIsLogin: (isLogin: boolean) => void;
+};
+
+export const AuthModal = ({
+  modalTitle,
+  buttonText,
+  children,
+  isOpen,
+  navigateText,
+  navigateModal,
+  scrollBehavior,
+  onClose,
+  modalType,
+  setModalType,
+  setIsLogin,
+}: AuthModalProps) => {
+  const onClick = () => {
+    if (modalType === 'login') {
+      setModalType('signup');
+    } else {
+      setModalType('login');
+    }
+  };
+
+  const onLogin = () => {
+    setIsLogin(true);
+    onClose();
+  };
+  return (
+    <Modal size="sm" isOpen={isOpen} onClose={onClose} scrollBehavior={scrollBehavior} isCentered>
+      <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(3px) hue-rotate(90deg)" />
+      <ModalContent w="100%">
+        <ModalHeader mt={5} textAlign="left">
+          {modalTitle}
+        </ModalHeader>
+        <ModalCloseButton />
+
+        <ModalBody textAlign="left">{children}</ModalBody>
+
+        <ModalFooter w="full" flexDir="column">
+          <Button w="full" h="40px" px="auto" colorScheme="custom.blue" onClick={onLogin}>
+            {buttonText}
+          </Button>
+          <Flex mt={5} mb={2} alignItems="center">
+            <Text fontSize="sm" color="custom.blue">
+              {navigateText}
+            </Text>
+            <Button bg="none" color="custom.blue" border="none" _hover={{}} onClick={onClick}>
+              {navigateModal}
+            </Button>
+          </Flex>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
