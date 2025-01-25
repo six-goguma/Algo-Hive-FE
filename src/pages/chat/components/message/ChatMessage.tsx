22fd0eed8bb0dc8336cadcbf76eed6ea8f52e789
Heeyeun <koheeyeun96@gmail.com>
2025-01-26 00:05:10 +0900

    style(chat): ChatMessage 디자인

--- src/pages/chat/components/message/ChatMessage.tsx
+++ src/pages/chat/components/message/ChatMessage.tsx
@@ -1,7 +1,21 @@
+import { Box, Flex, Image, Text } from '@chakra-ui/react';
+
+import userIcon from '@shared/_assets/userIcon.svg';
+
 export const ChatMessage = ({ message }: { message: { sender: string; content: string } }) => {
   return (
-    <div>
-      <strong>{message.sender}:</strong> {message.content}
-    </div>
+    <Box w='full' h='60px' bg='blue.100' mt='18px' position='relative'>
+      <Flex h='full' gap='7px' pl='8px' position='absolute' top='0'>
+        <Image src={userIcon} alt='userIcon' boxSize='30px' />
+        <Flex flexDir='column' align='center'>
+          <Text w='full' textAlign='left' fontSize='12px' fontWeight='semibold'>
+            {message.sender}
+          </Text>
+          <Box w='auto' h='38px' p='10px' bg='white' fontSize='12px' borderRadius='5px'>
+            {message.content}
+          </Box>
+        </Flex>
+      </Flex>
+    </Box>
   );
 };
