cc7005f9ccdd1995a9863faf525dac8e49f96485
Heeyeun <koheeyeun96@gmail.com>
2025-01-27 01:52:12 +0900

    style(mypage): MyPage 모바일 뷰 반응형 적용

--- src/pages/mypage/components/profile/UserProfileSection.tsx
+++ src/pages/mypage/components/profile/UserProfileSection.tsx
@@ -2,28 +2,47 @@ import { Avatar, Flex, Button, Text, Box } from '@chakra-ui/react';
 
 export const UserProfileSection = () => {
   const userNickname = 'gogumalatte';
+
   return (
-    <Flex w='80%' h='210px' mt='40px'>
-      <Flex w='128px' flexDir='column' align='center'>
-        <Avatar size='128px' mb='16px' />
-        <Button w='118px' h='40px' fontSize='18px' mb='10px'>
+    <Flex
+      w={{ base: '100%', sm: '80%' }}
+      h={{ base: 'auto', sm: '210px' }}
+      mt='40px'
+      flexDir={{ base: 'column', sm: 'row' }}
+      align={{ base: 'center', sm: 'flex-start' }}
+      p={{ base: '20px', sm: '0' }}
+    >
+      <Flex
+        w={{ base: 'full', sm: '128px' }}
+        flexDir='column'
+        align='center'
+        mb={{ base: '20px', sm: '0' }}
+      >
+        <Avatar boxSize={24} mb='16px' />
+        <Button w='110px' h='36px' fontSize='14px' mb='10px'>
           이미지 업로드
         </Button>
-        <Button bg='none' border='none' w='118px' h='40px' color='custom.blue' fontSize='18px'>
+        <Button bg='none' border='none' w='1180x' h='36px' color='custom.blue' fontSize='14px'>
           이미지 제거
         </Button>
       </Flex>
-      <Flex w='150px' flexDir='column' ml='30px' align='flex-start'>
-        <Text fontSize='24px' fontWeight='Bold' mb='6px' mt='10px'>
+
+      <Flex
+        w={{ base: 'full', sm: '150px' }}
+        flexDir='column'
+        ml={{ base: '0', sm: '30px' }}
+        align={{ base: 'center', md: 'flex-start' }}
+      >
+        <Text fontSize='20px' fontWeight='Bold' mb='6px' mt='10px'>
           {userNickname}
         </Text>
         <Box fontSize='16px' fontWeight='Bold' bg='none' border='none' cursor='pointer'>
           <Text
             w='full'
-            fontSize='16px'
+            fontSize='14px'
             fontWeight='Bold'
             lineHeight='16px'
-            textAlign='left'
+            textAlign='center'
             color='custom.blue'
             textDecoration='underline'
           >
@@ -31,8 +50,9 @@ export const UserProfileSection = () => {
           </Text>
         </Box>
       </Flex>
-      <Flex w='full' justify='flex-end'>
-        <Button bg='white' color='gray.500' w='80px' h='32px' fontSize='16px'>
+
+      <Flex w='full' justify={{ base: 'center', md: 'flex-end' }} mt={{ base: '20px', md: '0' }}>
+        <Button bg='white' color='gray.500' w='80px' h='32px' fontSize='14px'>
           회원탈퇴
         </Button>
       </Flex>
