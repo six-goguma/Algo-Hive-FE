461fd57228c04102f0b6418b4a85e7a63940b8c2
Dobbymin <gmin990624@gmail.com>
2025-01-27 22:45:32 +0900

    feat(main): 컴포넌트 분리 및 post-list 구현

--- src/pages/main/ui/MainPage.tsx
+++ src/pages/main/ui/MainPage.tsx
@@ -1,18 +1,12 @@
-import { Link } from 'react-router-dom';
-
 import { Flex, Skeleton, VStack } from '@chakra-ui/react';
 
 import { useGetMockData } from '@shared/hooks';
-import { getDynamicPath } from '@shared/utils';
 
-import { Grid } from '@widgets/grid';
-import { PostCards, SkeletonPostCards } from '@widgets/post-cards';
-
-import { Tabs } from '../components';
-import { POST_LIST_DUMMY_DATA } from '../mock';
+import { PostList, Tabs } from '../components';
+import { POST_LIST_DUMMY_DATA } from '../data';
 
 export const MainPage = () => {
-  const { data, isPending, isError } = useGetMockData(POST_LIST_DUMMY_DATA);
+  const { isPending, isError } = useGetMockData(POST_LIST_DUMMY_DATA);
 
   if (isError) return <div>오류가 발생했습니다.</div>;
 
@@ -26,24 +20,7 @@ export const MainPage = () => {
         ) : (
           <Tabs />
         )}
-        <Grid columns={{ base: 1, sm: 2, md: 3 }} gap={20}>
-          {isPending
-            ? Array.from({ length: 6 }).map((_, index) => <SkeletonPostCards key={index} />)
-            : data.content.map((post) => (
-                <Link key={post.id} to={getDynamicPath.postDetail(String(post.id))}>
-                  <PostCards
-                    title={post.title}
-                    postId={post.id}
-                    thumbnail={post.thumbnail}
-                    summary={post.summary}
-                    createdAt={post.createdAt}
-                    likeCount={post.likeCount}
-                    commentCount={post.commentCount}
-                    author={post.author}
-                  />
-                </Link>
-              ))}
-        </Grid>
+        <PostList />
       </VStack>
     </Flex>
   );
