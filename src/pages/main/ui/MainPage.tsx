d3752b924cb1b9a5d6c1a5138c709b132979329e
Dobbymin <gmin990624@gmail.com>
2025-01-27 23:11:03 +0900

    feat(main): 버튼 컴포넌트 적용 및 skeleton 컴포넌트 적용

--- src/pages/main/ui/MainPage.tsx
+++ src/pages/main/ui/MainPage.tsx
@@ -1,8 +1,8 @@
-import { Flex, Skeleton, VStack } from '@chakra-ui/react';
+import { Flex, VStack } from '@chakra-ui/react';
 
 import { useGetMockData } from '@shared/hooks';
 
-import { PostList, Tabs } from '../components';
+import { ButtonSkeleton, NavigateButton, PostList, Tabs } from '../components';
 import { POST_LIST_DUMMY_DATA } from '../data';
 
 export const MainPage = () => {
@@ -14,11 +14,12 @@ export const MainPage = () => {
     <Flex w='full' justifyContent='center'>
       <VStack spacing={10} pb={10}>
         {isPending ? (
-          <Flex w='full' py={8}>
-            <Skeleton w='160px' h={10} />
-          </Flex>
+          <ButtonSkeleton />
         ) : (
-          <Tabs />
+          <Flex w='full' justifyContent='space-between'>
+            <Tabs />
+            <NavigateButton />
+          </Flex>
         )}
         <PostList />
       </VStack>
