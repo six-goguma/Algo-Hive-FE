e345cd79949ed1724261ae47249ae6ee67b5e25f
kiyeong <kjr07145@gmail.com>
2025-01-31 14:19:13 +0900

    feat(post-write): post-form 컴포넌트 재사용하여 게시글 작성 페이지 구현

--- src/pages/post-write/ui/PostWritePage.tsx
+++ src/pages/post-write/ui/PostWritePage.tsx
@@ -1,25 +1,58 @@
-import { useDisclosure, Button } from '@chakra-ui/react';
+import { Button, useDisclosure, Box, Flex, VStack } from '@chakra-ui/react';
+
+import { ArrowLeft } from 'lucide-react';
+
+import { PostTitle, PostTag, PostContent } from '@shared/components';
 
 import { PostModal } from '@widgets/modals';
 
 export const PostWritePage = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  //TODO: 작성완료 버튼 클릭 시 Modal 열기
   const onClick = () => {
     onOpen();
   };
 
   return (
-    <div>
-      <Button onClick={onClick}>작성완료</Button>
-      <PostModal
-        title='백준 1004번 풀이'
-        isOpen={isOpen}
-        onClose={onClose}
-        buttonTitle='출간하기'
-        postType='create'
-      />
-    </div>
+    <Flex w='full' justifyContent='center'>
+      <VStack w='full' py={10} spacing='0'>
+        <PostTitle />
+        <Box w='full' h='8px' bg='white'>
+          <Box w='115px' h='8px' bg='#495057' ml='50px' />
+        </Box>
+        <PostTag />
+        <PostContent />
+        <Flex
+          w='full'
+          h='50px'
+          px='50px'
+          mt={1}
+          background='white'
+          alignItems='center'
+          justifyContent='space-between'
+        >
+          <Button
+            variant='ghost'
+            colorScheme='gray'
+            fontSize='md'
+            fontWeight='normal'
+            leftIcon={<ArrowLeft />}
+            _hover={{ bg: 'transparent' }}
+          >
+            나가기
+          </Button>
+          <Button colorScheme='blue' onClick={onClick} fontWeight='medium'>
+            작성완료
+          </Button>
+        </Flex>
+        <PostModal
+          title='백준 1004번 풀이'
+          isOpen={isOpen}
+          onClose={onClose}
+          buttonTitle='출간하기'
+          postType='create'
+        />
+      </VStack>
+    </Flex>
   );
 };
