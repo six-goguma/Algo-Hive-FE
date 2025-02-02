3fb3cbb0f257e912151bfc409335e4fc519817c6
kiyeong <kjr07145@gmail.com>
2025-02-03 02:07:32 +0900

    feat: 게시글 작성 페이지 스타일 수정

--- src/pages/post-write/ui/PostWritePage.tsx
+++ src/pages/post-write/ui/PostWritePage.tsx
@@ -1,4 +1,4 @@
-import { Button, useDisclosure, Box, Flex, VStack } from '@chakra-ui/react';
+import { Button, useDisclosure, Box, Flex, VStack, Divider } from '@chakra-ui/react';
 
 import { ArrowLeft } from 'lucide-react';
 
@@ -9,39 +9,34 @@ import { PostModal } from '@widgets/modals';
 export const PostWritePage = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const onClick = () => {
-    onOpen();
-  };
-
   return (
     <Flex w='full' justifyContent='center'>
-      <VStack w='full' py={10} spacing='0'>
+      <VStack w='full' py='40px' gap='0'>
         <PostTitle />
-        <Box w='full' h='8px' bg='white'>
-          <Box w='115px' h='8px' bg='#495057' ml='50px' />
+        <Box w='full' h='14px' bg='white' py='3px'>
+          <Divider w='115px' h='8px' bg='black' ml='50px' />
         </Box>
         <PostTag />
         <PostContent />
         <Flex
           w='full'
           h='50px'
-          px='50px'
-          mt={1}
-          background='white'
-          alignItems='center'
-          justifyContent='space-between'
+          px={{ base: '10px', sm: '30px' }}
+          mt='5px'
+          bg='white'
+          align='center'
+          justify='space-between'
         >
           <Button
             variant='ghost'
             colorScheme='gray'
             fontSize='md'
-            fontWeight='normal'
             leftIcon={<ArrowLeft />}
             _hover={{ bg: 'transparent' }}
           >
             나가기
           </Button>
-          <Button colorScheme='blue' onClick={onClick} fontWeight='medium'>
+          <Button variant='solid' colorScheme='blue' py={4} fontSize='md' onClick={onOpen}>
             작성완료
           </Button>
         </Flex>
