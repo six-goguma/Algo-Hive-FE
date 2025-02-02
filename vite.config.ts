d8b928b68fa2db3a1f8d1e3d693c939d60842933
Dobbymin <gmin990624@gmail.com>
2025-02-02 17:17:44 +0900

    fix: chakra ui v3 업데이트 및 기타 세팅 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 import react from '@vitejs/plugin-react';
 
@@ -7,7 +8,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       '@app': path.resolve(__dirname, 'src/app'),
