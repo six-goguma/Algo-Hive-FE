7299fd01e9701a5f72f447d656684140a16a1dcb
Dobbymin <gmin990624@gmail.com>
2025-02-02 17:18:13 +0900

    refactor: chakra ui provider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Routes } from '@app/routes';
 
+import { queryClient } from '@shared/lib';
 import { ApplicationProvider } from '@shared/providers';
+
+import { QueryClientProvider } from '@tanstack/react-query';
 
 const App = () => {
   return (
     <ApplicationProvider>
-      <Routes />
+      <QueryClientProvider client={queryClient}>
+        <Routes />
+      </QueryClientProvider>
     </ApplicationProvider>
   );
 };
