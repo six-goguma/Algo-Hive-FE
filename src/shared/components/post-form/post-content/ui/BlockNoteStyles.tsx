57e00e52fc9db23355d5e7faead37a0c77bee153
kiyeong <kjr07145@gmail.com>
2025-02-03 05:07:51 +0900

    chore: 게시글 컴포넌트 주석 및 fragment 제거

--- src/shared/components/post-form/post-content/ui/BlockNoteStyles.tsx
+++ src/shared/components/post-form/post-content/ui/BlockNoteStyles.tsx
@@ -3,11 +3,11 @@ export const BlockNoteStyles = () => {
     <style>
       {`
           .bn-editor {
-              padding-inline: 50px; /* 기본(데스크탑)에서는 50px */
+              padding-inline: 50px;
           }
           @media (max-width: 768px) {
               .bn-editor {
-                  padding-inline: 10px; /* 모바일에서는 10px */
+                  padding-inline: 10px;
               }
           }
         `}
