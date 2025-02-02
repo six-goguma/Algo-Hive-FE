e85598bf4bbfca81a62a59b66f57c710879e3834
kiyeong <kjr07145@gmail.com>
2025-02-03 02:03:00 +0900

    feat: 게시글 수정 페이지 스타일 수정

--- src/pages/post-edit/ui/PostEditPage.tsx
+++ src/pages/post-edit/ui/PostEditPage.tsx
@@ -1,4 +1,4 @@
-import { Button, useDisclosure, Box, Flex, VStack } from '@chakra-ui/react';
+import { Button, useDisclosure, Box, Flex, VStack, Divider } from '@chakra-ui/react';
 
 import { ArrowLeft } from 'lucide-react';
 
@@ -15,46 +15,38 @@ export const PostEditPage = () => {
 
   const { data, isPending, isError } = useGetMockData(POST_DETAIL_DUMMY_DATA);
 
-  //TODO: 게시글 수정하기 버튼 클릭 시 Modal 열기
-  const onClick = () => {
-    onOpen();
-  };
-
-  //TODO: 게시글 data 불러오는 중에 에러 발생 시 custom Toast 메시지 띄우기
   if (isError) return <div>오류가 발생했습니다.</div>;
 
-  //TODO: 게시글 data 불러올 떄 Skeleton으로 변경
   if (isPending) return <LoadingView />;
 
   return (
     <Flex w='full' justifyContent='center'>
-      <VStack w='full' py={10} spacing='0'>
+      <VStack w='full' py='40px' gap='0'>
         <PostTitle />
-        <Box w='full' h='8px' bg='white'>
-          <Box w='115px' h='8px' bg='#495057' ml='50px' />
+        <Box w='full' h='14px' bg='white' py='3px'>
+          <Divider w='115px' h='8px' bg='black' ml='50px' />
         </Box>
         <PostTag />
         <PostContent />
         <Flex
           w='full'
           h='50px'
-          px='50px'
-          mt={1}
-          background='white'
-          alignItems='center'
-          justifyContent='space-between'
+          px={{ base: '10px', sm: '30px' }}
+          mt='5px'
+          bg='white'
+          align='center'
+          justify='space-between'
         >
           <Button
             variant='ghost'
             colorScheme='gray'
             fontSize='md'
-            fontWeight='normal'
             leftIcon={<ArrowLeft />}
             _hover={{ bg: 'transparent' }}
           >
             나가기
           </Button>
-          <Button colorScheme='blue' onClick={onClick} fontWeight='medium'>
+          <Button variant='solid' colorScheme='blue' py={4} fontSize='md' onClick={onOpen}>
             수정하기
           </Button>
         </Flex>
