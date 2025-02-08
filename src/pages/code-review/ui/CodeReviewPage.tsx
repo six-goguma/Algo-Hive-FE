84de9f82fb7cc402d7fc3ecda4be8d728e282d73
Heeyeun <koheeyeun96@gmail.com>
2025-02-09 00:45:47 +0900

    style(code-review): 모바일 뷰 수정

--- src/pages/code-review/ui/CodeReviewPage.tsx
+++ src/pages/code-review/ui/CodeReviewPage.tsx
@@ -5,7 +5,7 @@ import { CodeInputBox, ReviewResult } from '../components';
 export const CodeReviewPage = () => {
   return (
     <Flex w='full' justify='center' overflow={{ base: 'scroll', sm: 'hidden' }}>
-      <Flex w='80%' flexDir='column' align='center' mb='100px'>
+      <Flex w={{ base: 'full', sm: '80%' }} flexDir='column' align='center' mb='100px'>
         <CodeInputBox />
         <ReviewResult />
       </Flex>
