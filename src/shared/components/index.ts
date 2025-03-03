abf6bee4675e4385a2262e38257d98ce75eeda1c
Dobbymin <gmin990624@gmail.com>
2025-03-03 16:30:09 +0900

    remove: 사용하지 않는 구조 제거

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -2,7 +2,5 @@ export * from './layouts';
 export * from './post-form';
 export * from './pagination';
 export * from './error';
-export * from './buttons';
 export * from './form';
 export * from './input';
-export * from './buttons';
